171231cef7b3988bd1c7faa5f5a3cb8f6aa73781
Jonathan Lam <jlam55555@gmail.com>
2018-12-01 14:48:11 -0500

    updated files

--- app/src/main/java/com/herokuapp/accelomap/opengltest/MyGLRenderer.java
+++ app/src/main/java/com/herokuapp/accelomap/opengltest/MyGLRenderer.java
@@ -8,6 +8,22 @@ import android.view.SurfaceView;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+  private Triangle mTriangle;
+
+  public static int loadShader(int type, String shaderCode){
+
+    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+    int shader = GLES20.glCreateShader(type);
+
+    // add the source code to the shader and compile it
+    GLES20.glShaderSource(shader, shaderCode);
+    GLES20.glCompileShader(shader);
+
+    return shader;
+  }
+
   @Override
   public void onSurfaceCreated(GL10 gl10, javax.microedition.khronos.egl.EGLConfig eglConfig) {
     // Set the background frame color
@@ -18,6 +34,8 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
   public void onDrawFrame(GL10 unused) {
     // Redraw background color
     GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+    mTriangle.draw();
   }
 
   @Override
